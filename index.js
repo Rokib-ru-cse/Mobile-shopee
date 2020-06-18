8fc02a0155c903128d7293937f9a3ab1d35c922f
Rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2020-06-18 09:59:52 +0600

    complete new phones sections

--- index.js
+++ index.js
@@ -39,5 +39,23 @@ $(document).ready(function(){
 
     })
 
+    //New Phones owl carousel
+    $("#new-phones .owl-carousel").owlCarousel({
+        loop:true,
+        nav:false,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
 });
 
