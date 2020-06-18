d103c6e0680409e51ce14a1dcb406cfeaf089d7b
Rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2020-06-18 09:40:50 +0600

    complete special price section

--- index.js
+++ index.js
@@ -23,5 +23,21 @@ $(document).ready(function(){
             }
         }
     });
+
+    //------------isotope
+    var $grid = $(".grid").isotope({
+        itemSelector:'.grid-item',
+        layoutMode:'fitRows'
+    })
+
+    //filte item on button click
+    $(".button-group").on("click","button",function(){
+        var filterValue = $(this).attr("data-filter");
+        $grid.isotope({
+            filter:filterValue
+        });
+
+    })
+
 });
 
